0ba66df0f89328a241eeb1c45cce16b5e4b047ab
Ed Connolly <edmond.connolly@webinnovate.ie>
2016-11-27 01:13:12 +0000

    new header

--- includes/header.inc.php
+++ includes/header.inc.php
@@ -27,8 +27,8 @@ ob_start();
   <meta name="twitter:widgets:autoload" content="on">
   <link rel="canonical" href="https://dev.twitter.com/">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-	<link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
-  <link rel="stylesheet" href="css/styles.css">
+<!--   <link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+   -->  <link rel="stylesheet" href="css/styles.css">
   <script src="js/edcodev.js" type="text/javascript"></script>
 	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
 	<!-- <title><?php  $page_title; ?></title> -->
@@ -44,13 +44,99 @@ ob_start();
   </head>
 <body>
   <header class="woops">
+    <div class="container">
+  <nav class="nav navbar-default">
+     <div class="container">
+    <!-- Brand and toggle get grouped for better mobile display -->
+        
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li><a href="index.php">Home</a></li>
+        <li><a href="#">Link</a></li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <?php if ($user && $user->canCreatePage()) echo '<li><a href="add_page.php">Add a New Page</a></li>'; ?>
+            <li><a href="#">Another action</a></li>
+            <li><a href="#">Something else here</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">Separated link</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">One more separated link</a></li>
+          </ul>
+        </li>
+      </ul>
+    
+      <ul class="nav navbar-nav navbar-right">
+        <?php 
+                // Display links based upon the login status:
+            /*    if (isset($_SESSION['user_id'])) {
+
+                  echo '<li><a href="logout.php" title="Logout">Logout</a></li>
+                <li><a href="change_password.php" title="Change Your Password">Change Password</a></li>
+                ';
+
+                  // Add links if the user is an administrator:
+                  if ($_SESSION['user_level'] == 1) {
+                    echo '<li><a href="view_users.php" title="View All Users">View Users</a></li>
+                  <li><a href="#">Some Admin Page</a></li>
+                  ';
+                  }*/
+                
+                if (!isset($_SESSION['user_id'])) { //  Not logged in.
+                  echo '<li><a href="register.php" title="Register for the Site">Register</a></li>
+                <li><a href="forgot_password.php" title="Password Retrieval">Retrieve Password</a></li>
+                ';
+                }
+                ?>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><?php if (isset($_SESSION['first_name'])) {
+  echo "<span class='test'> {$_SESSION['first_name']}</span>";} else { //  Not logged in.
+                  echo '<li><a href="login.php" title="Login">Log in</a></li>';
+                }
+                ?>
+ <span class="caret"></span> </a> 
+          <ul class="dropdown-menu">
+            <?php 
+                // Display links based upon the login status:
+                if (isset($_SESSION['user_id'])) {
+
+                  echo '<li><a href="logout.php" title="Logout">Logout</a></li>
+                <li><a href="change_password.php" title="Change Your Password">Change Password</a></li>
+                ';
+
+                  // Add links if the user is an administrator:
+                  if ($_SESSION['user_level'] == 1) {
+                    echo '<li role="separator" class="divider"></li>
+                    <li><a href="view_users.php" title="View All Users">View Users</a></li>
+                  <li><a href="#">Some Admin Page</a></li>
+                  ';
+                  }
+                
+                } else { //  Not logged in.
+                  echo '<li><a href="register.php" title="Register for the Site">Register</a></li>
+                <li><a href="login.php" title="Login">Log in</a></li>
+                <li><a href="forgot_password.php" title="Password Retrieval">Retrieve Password</a></li>
+                ';
+                }
+                ?>
+            
+          </ul>
+        </li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+</div>
     
 
 
       <div class="container">
         <div class="row-fluid">
           <div class="col-md-3">
-            <img src="images/ed-connolly-LOGO-01.png" alt="" />
+            <img src="assets/img/ed-connolly-LOGO-01.png" alt="" />
           </div>
         </div>
       </div>
@@ -65,17 +151,17 @@ ob_start();
      <div class="container">
     <!-- Brand and toggle get grouped for better mobile display -->
         <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#naCol" aria-expanded="false">
             <span class="sr-only">Toggle navigation</span>
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="#">Brand</a>
+          
         </div>
 
     <!-- Collect the nav links, forms, and other content for toggling -->
-    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+    <div class="collapse navbar-collapse" id="naCol">
       <ul class="nav navbar-nav">
         <li><a href="index.php">Home</a></li>
         <li><a href="#">Link</a></li>
@@ -160,6 +246,21 @@ ob_start();
 </nav>
 </div>
 </div>
+
+<div class="container-fluid end1">
+  <div class="container realHdr1">
+  <nav class="nav navbar-inverse1">
+     <div class="container">
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li><a href="index.php">Home</a></li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+</div>
+</div>
+
 <div class="jquery-script-clear"></div>
 
 
